7af8874b5d0114f3a7f99f05f3e24ee892d99586
unknown <omrkmal2828@gmail.com>
2021-04-19 12:14:13 +0200

    learn vue.js

---
+++ src/components/WellcomeMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wellcom-message">
+    <h2>Wellcom to vue js</h2>
+    <p>my Name is omar kmal</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name : "WellcomeMessage",
+}
+</script>
+
+<style>
+  h2 {
+    font-size : 40px;
+  }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <wellcome-message/>
+  </div>
+</template>
+
+<script>
+
+import WellcomeMessage from '../components/WellcomeMessage.vue'
+export default {
+  components: WellcomeMessage }
+</script>
